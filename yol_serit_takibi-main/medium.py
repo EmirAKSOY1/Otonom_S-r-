58d60941acc11d81fa510a61caa1a370137b540e
EmirAKSOY1 <72320002+EmirAKSOY1@users.noreply.github.com>
2022-04-19 22:51:16 +0300

    Add files via upload

---
+++ yol_serit_takibi-main/medium.py
@@ -0,0 +1,11 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
+img = cv2.imread("medium.jpeg")
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+(thresh, output2) = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)
+output2 = cv2.GaussianBlur(output2, (5, 5), 3)
+output2 = cv2.Canny(output2, 180, 255)
+plt.imshow(output2)
+plt.show()
